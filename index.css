3522591807d267b3779359c014deee6ebc50edbc
mahesh kale <140434159+malharkale@users.noreply.github.com>
2023-12-13 10:22:56 +0530

    index.css

--- index.css
+++ index.css
@@ -1,1 +1,82 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <img id="large-image" class="image" src="https://cdn.shopify.com/s/files/1/0564/3685/0790/files/laura-chouette-6Y2XstWtDvM-unsplash.jpg?v=1701946731" />
+            <div class="bottom-image-container">
+                <div id="first" onclick="firstChange()" class="small-image">
+                    <img class="img" src="https://cdn.shopify.com/s/files/1/0564/3685/0790/files/laura-chouette-6Y2XstWtDvM-unsplash.jpg?v=1701946731" />
+                </div>
+                <div id="second" onclick="secondChange()" class="small-image">
+                    <img class="img" src="https://cdn.shopify.com/s/files/1/0564/3685/0790/files/laura-chouette-HVlOLCHlzJs-unsplash.jpg?v=1701946732" />
+                </div>
+                <div id="third" onclick="thirdChange()" class="small-image">
+                    <img class="img" src="https://cdn.shopify.com/s/files/1/0564/3685/0790/files/laura-chouette-om8qxMDlGfI-unsplash.jpg?v=1701946732" />
+                </div>
+                <div id="fourth" onclick="fourthChange()" class="small-image">
+                    <img class="img" src="https://cdn.shopify.com/s/files/1/0564/3685/0790/files/laura-chouette-WQgvRkmqRrg-unsplash.jpg?v=1701946731" />
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <p class="marmeto-heading">Marmeto</p>
+            <h1 class="embrace-highlight">Embrace Sideboard</h1>
+            <hr width="100%" color="#454545" />
+            <div>
+                <div class="embrace-container">
+                    <div class="price-container">
+                        <h1 class="price">$<span id="price">12999.00</span></h1>
+                        <p class="off-discount"> <span id="discount">35</span>%off</p>
+                    </div>
+                    <p class="discount-price" onclick="buttonClick()">$<span id="result">19999.00</span></p>
+                </div>
+            </div>
+            <hr width="100%" color="#454545" />
+            <p class="marmeto-heading">choose a color</p>
+            <div class="color-container">
+
+                <button id="box1" onclick="box1()" class=" block yellow"></button>
+                <button id="box2" onclick="box2()" class="block green"></button>
+                <button id="box3" onclick="box3()" class="block blue"></button>
+                <button id="box4" onclicj="box4()" class="block pink"></button>
+            </div>
+            <hr width="100%" color="#454545" />
+            <p class="marmeto-heading">Choose a Size </p>
+            <div class="input-box">
+                <input type="radio" id="small" onclick="smallSize()" name="fav_language" value="HTML">
+                <label for="html">Small</label>
+                <input type="radio" id="medium" onclick="mediumSize()" name="fav_language" value="HTML">
+                <label for="html">mediam</label>
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                <label id="large" onclick="LargeSize()" for="html">Large</label>
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                <label id="extraLarge" onclick="extralargeSize()" for="html">Extra Large</label>
+                <input type="radio" id="html" name="fav_language" value="HTML">
+                <label id="xxl" onclick="xxlSize()" for="html">XXL</label>
+            </div>
+            <div class="button-container">
+                <div>
+                    <button onClick="onDecrement()" class="button">-</button>
+                    <button id="counterValue" class="counter-value">1</button>
+                    <button onClick="onIncrement()" class="button">+</button>
+                </div>
+                <button class="add-button" onclick="addtoCart()"> <i class="fa fa-shopping-bag"></i> Add To Cart</button>
+            </div>
+            <div>
+                <p class="add-button-text" id="add-button"></p>
+            </div>
+            <hr width="100%" color="#454545" />
+            <p class="marmeto-heading paragraph">The Embrace Sideboard is a stylish wear. With a top cloth designed to provide superior protection and look great, this storage solution is both functional and attractive. It fits seamlessly into any home decor, with clean lines and a timeless look. Crafted from premium materials for a combination of style, durability, and reliability</p>
+        </div>
+    </div>
+</body>
+
+</html>
